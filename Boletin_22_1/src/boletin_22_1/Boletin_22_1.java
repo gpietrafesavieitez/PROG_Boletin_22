bbad18099186d410a134817eade9ba530e9ca4bd
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-02-11 20:29:18 +0100

    First commit

---
+++ Boletin_22_1/src/boletin_22_1/Boletin_22_1.java
@@ -0,0 +1,11 @@
+/**
+ * @author Gabriel <gpietrafesavieitez@danielcastelao.org>
+ */
+
+package boletin_22_1;
+
+public class Boletin_22_1 {
+
+    public static void main(String[] args) {
+    }
+}
